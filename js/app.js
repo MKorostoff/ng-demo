af872db0e5470d7a6044bd9354f0956beb6a62a1
Matt Korostoff <matt@blinkreaction.com>
2016-06-06 14:39:16 -0400

    Make list of names dynamic

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 var app = angular.module('MyDemoApp', []);
 
-app.controller('athleteSearch', function($scope) {
-  $scope.lastName = 'Korostoff';
-  $scope.shortName = 'Matt';
-  $scope.longName = 'Matthew';
+app.controller('athleteSearch', function($scope, $http, $window) {
   
-  $scope.short = false;
-  if (window.innerWidth < 800) {
-    $scope.short = true;
+  $http({
+    method: 'GET',
+    url: '/data/names.php'
+  }).then(function successCallback(response) {
+    $scope.names = response.data
+  });
+  
+  $scope.resize = function() {
+    console.log('foo');
+    $scope.short = false;
+    if (window.innerWidth < 800) {
+      $scope.short = true;
+    }
   }
+  
+  $scope.resize();
+  angular.element($window).bind('resize', function() {
+    $scope.resize();
+    $scope.$apply();
+  });
+
 });
+
