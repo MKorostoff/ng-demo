43384b4060043f0c15bfa975e64320bdc9a8a3b2
Matt Korostoff <matt@blinkreaction.com>
2016-06-06 12:13:45 -0400

    Show short names on browser resize

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var app = angular.module('MyDemoApp', []);
 
 app.controller('athleteSearch', function($scope) {
-  $scope.firstName = 'Matt';
   $scope.lastName = 'Korostoff';
+  $scope.shortName = 'Matt';
+  $scope.longName = 'Matthew';
+  
+  $scope.short = false;
+  if (window.innerWidth < 800) {
+    $scope.short = true;
+  }
 });
