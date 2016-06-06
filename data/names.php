7ce9e8897ea415fe0d6aad4a1302695307481077
Matt Korostoff <matt@blinkreaction.com>
2016-06-06 14:59:58 -0400

    Add data directory

---
+++ data/names.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'vendor/autoload.php';
+$faker = Faker\Factory::create();
+
+$result = array();
+for ($i=0; $i < 120; $i++) { 
+  $first = $faker->firstName;
+  $result[] = array(
+    'longName' => $first,
+    'shortName' => substr($first, 0, 3),
+    'lastName' => $last = $faker->lastName,
+  );
+}
+print json_encode($result);
